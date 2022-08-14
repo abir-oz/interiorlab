fe31795834e11a7802c61dec9a74183c68141264
abir-oz <jahirul15-8163@diu.edu.bd>
2022-08-15 04:06:34 +0600

    Done with projects section: 15/08/2022 - 4:06 AM

--- src/components/home/Projects.js
+++ src/components/home/Projects.js
@@ -3,13 +3,13 @@ import { projectData } from '../../data/postData';
 const Projects = () => (
 
     <div className="max-w-screen my-10">
-        <div className='flex flex-wrap md:mx-10 lg:mx-16 mx-4 justify-center items-center'>{
+        <div className='flex flex-wrap md:mx-8 xl:mx-16 mx-4 justify-center items-center'>{
             projectData.map((item, index) => (
                 <div key={index} className="group lg:w-1/3 sm:w-1/2 w-full relative">
-                    <div className='m-2 lg:m-10 text-center'>
+                    <div className='m-2 lg:m-8 text-center'>
                         <div className='w-full h-full overflow-hidden relative'>
                             <img className='group-hover:scale-125 transition-all duration-1000 ease-in-out transform z-10' src={item.thumb} alt={item.title} />
-                            <a className='border-2 py-2 px-3 m-2 absolute z-50 transition ease-out delay-100 duration-700 group-hover:-translate-y-36  -bottom-8 right-36 invisible group-hover:visible text-white' href={`/${item.title}`}>{item.buttonText}</a>
+                            <a className='border-2 py-2 px-3 m-2 absolute transition ease-out delay-100 duration-700 bottom-0 right-28 md:right-24 lg:-bottom-6 lg:right-16  xl:bottom-6  xl:right-32 group-hover:-translate-y-20 invisible group-hover:visible text-white z-50' href={`/${item.title}`}>{item.buttonText}</a>
                             <div className='w-full h-full top-0 left-0 border bg-orange-500 opacity-0 group-hover:opacity-80 transition ease-out delay-150 duration-300 z-10 absolute'></div>
                         </div>
 
