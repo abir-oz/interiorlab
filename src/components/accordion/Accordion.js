90b65e38f2355835cb8ce0c71367c9045d1f9358
abir-oz <jahirul15-8163@diu.edu.bd>
2022-09-14 23:02:18 +0600

    Trying to solve vercel deployment issue

---
+++ src/components/accordion/Accordion.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AccordionItem from './AccordionItem';
+
+const Accordion = () => {
+    return (
+        <div className="m-h-screen my-10 grid place-items-center">
+            <h3 className='uppercase m-0 font-bold font-display text-2xl tracking-wide'>FAQ</h3>
+            <div className="w-10/12 mx-auto">
+                <div className="bg-white md:p-10">
+                    <AccordionItem />
+                    <AccordionItem />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Accordion;
---
+++ src/components/accordion/AccordionItem.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { FaChevronUp } from 'react-icons/fa';
+
+const AccordionItem = () => {
+    const [isActive, setIsActive] = useState(false);
+    const [maxHeight, setMaxHeight] = useState("max-h-0");
+    const clickHandler = (e) => {
+        e.preventDefault();
+        setIsActive((prevState) => !prevState);
+
+        isActive ? setMaxHeight("max-h-0") : setMaxHeight("max-h-96");
+    }
+    return (
+        <div className="transition my-1">
+            <div onClick={clickHandler} className="cursor-pointer transition flex bg-gray-50 space-x-5 md:p-4 p-2 m-2 items-center max-h-32">
+                <span>{isActive ? <FaChevronUp className='md:text-lg text-xs' /> : <FaChevronUp className='md:text-lg text-xs -scale-y-90 transition-all duration-300' />}</span>
+                <h3> What is term?</h3>
+            </div>
+
+            <div className={`accordion-content px-5 pt-0 overflow-hidden ${maxHeight}`}>
+                <p className="leading-6 font-light pl-9 text-justify">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis dolore nulla accusamus vel voluptates. Veritatis officia fuga esse sapiente repudiandae?
+                </p>
+                <button className="rounded-full bg-indigo-600 text-white font-medium font-lg px-6 py-2 my-5 ml-9">Learn more</button>
+            </div>
+        </div >
+    );
+};
+
+export default AccordionItem;
